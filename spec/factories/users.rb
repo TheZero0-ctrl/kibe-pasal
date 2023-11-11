cb9cfa71fd9dea44d003bb2761f895bcd97904cf
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-11-11 21:02:32 +0545

    add has_secure_password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     name { 'Name' }
     sequence(:email) { |n| "test#{n}@mail.com" }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,26 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:memberships).dependent(:destroy) }
     it { is_expected.to have_many(:organizations).through(:memberships) }
+    it { is_expected.to have_secure_password }
   end
 
   describe 'validations' do
+    max_length = ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED
     it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    it { is_expected.to validate_length_of(:password).is_at_most(max_length) }
 
     it 'email should be uniq' do
       create(:user)
       expect(subject).to validate_uniqueness_of(:email).case_insensitive
+    end
+  end
+
+  describe 'callbacks' do
+    it 'stripped the spaces from email and name before saving' do
+      user = create(:user, name: ' test ', email: ' new@mail.com ')
+      expect(user.name).to eq('test')
     end
   end
 end
